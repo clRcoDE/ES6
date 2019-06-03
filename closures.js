01056157ce9813c65481de2aecac6a85790ed3b6
clRcoDE <45456252+clRcoDE@users.noreply.github.com>
2019-06-03 15:34:39 +0430

    initial commit

---
+++ closures.js
@@ -0,0 +1,37 @@
+
+
+
+
+outer = () =>{
+    let message = 'howdy'
+    greet =() =>{
+        console.log(message)
+    }
+    return greet
+}
+
+outer()
+
+let outerFunc = outer()
+outerFunc()
+
+console.log(outer())
+
+
+
+
+ function createCounter() {
+       let counter = 0
+       const myFunction = function() {
+         counter = counter + 1
+         return counter
+       }
+       return myFunction
+     }
+     const increment = createCounter()
+    const c1 = increment()
+    const c2 = increment()
+    const c3 = increment()
+
+
+    console.log('example increment', c1, c2, c3)
